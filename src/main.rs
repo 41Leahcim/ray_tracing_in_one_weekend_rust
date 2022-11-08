822577f31e039a57088542ac47b0198ce25eb77a
Michael Scholten <michael-scholten@hotmail.nl>
2022-11-08 16:10:07 +0100

    2.1 The PPM Image Format

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    // Image dimensions
+    const IMAGE_WIDTH: usize = 256;
+    const IMAGE_HEIGHT: usize = 256;
+
+    // Render the image
+    println!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255");
+    for y in (0..IMAGE_HEIGHT).rev(){
+        for x in 0..IMAGE_WIDTH{
+            let red = x as f64 / (IMAGE_WIDTH - 1) as f64;
+            let green = y as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let blue = 0.25;
+            let red = (255.999 * red) as u8;
+            let green = (255.999 * green) as u8;
+            let blue = (255.999 * blue) as u8;
+            println!("{red} {green} {blue}"); 
+        }
+    }
 }
