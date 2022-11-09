c3362a81ca428462df9d4328f2418e4b66730f62
Michael Scholten <michael-scholten@hotmail.nl>
2022-11-09 15:58:30 +0100

    5. Adding a sphere

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,19 @@ mod vec3;
 mod color;
 mod ray;
 
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool{
+    let oc = r.origin() - center;
+    let a = r.direction().dot(r.direction());
+    let b = 2.0 * oc.dot(r.direction());
+    let c = radius.mul_add(-radius, oc.dot(oc));
+    let discriminant = b.mul_add(b,- 4.0 * a * c);
+    discriminant > 0.0
+}
+
 fn ray_color(r: &Ray) -> Color{
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r){
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = r.direction().unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
     mul(1.0 - t, Color::new(1.0, 1.0, 1.0)) + mul(t, Color::new(0.5, 0.7, 1.0))
