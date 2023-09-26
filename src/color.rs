29d7dd7476b4e0528d56f866df3981d94398fb70
Michael Scholten <michael-scholten@hotmail.nl>
2023-09-26 14:20:54 +0200

    Made the output argument of color::write accept everything implementing io::Write

--- src/color.rs
+++ src/color.rs
@@ -1,10 +1,10 @@
-use std::io::{self, BufWriter, Write};
+use std::io;
 
 use crate::vec3::Vec3;
 
 pub type Color = Vec3;
 
-pub fn write<W: std::io::Write>(out: &mut BufWriter<W>, color: Color) -> io::Result<()> {
+pub fn write<W: io::Write>(out: &mut W, color: Color) -> io::Result<()> {
     let red = (255.999 * color.x()) as u8;
     let green = (255.999 * color.y()) as u8;
     let blue = (255.999 * color.z()) as u8;
