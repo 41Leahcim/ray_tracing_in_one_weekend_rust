8b296714dde760bc93f01e30c4740a1cd5f2b2af
Michael Scholten <michael-scholten@hotmail.nl>
2025-04-11 19:45:13 +0200

    Added Vec3, Point3, and Color and allowed them to turn into Rgb colors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use image::{ImageBuffer, Rgb};
+use vec3::Color;
+
+pub mod vec3;
 
 fn main() {
     const IMAGE_WIDTH: u32 = 16382;
@@ -7,10 +10,12 @@ fn main() {
         if x == 0 {
             eprint!("\rScanlines remaining: {} ", IMAGE_HEIGHT - y);
         }
-        let red = f64::from(x) / f64::from(IMAGE_WIDTH - 1);
-        let green = f64::from(y) / f64::from(IMAGE_HEIGHT - 1);
-        let blue = 0;
-        Rgb([(red * 255.999) as u8, (green * 255.999) as u8, blue])
+        let pixel_color = Color::new([
+            f64::from(x) / f64::from(IMAGE_WIDTH - 1),
+            f64::from(y) / f64::from(IMAGE_HEIGHT - 1),
+            0.0,
+        ]);
+        Rgb::from(pixel_color)
     })
     .save("image.ppm")
     .unwrap();
---
+++ src/vec3.rs
@@ -0,0 +1,194 @@
+use std::{
+    fmt::Display,
+    ops::{Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Neg, Sub, SubAssign},
+};
+
+use image::Rgb;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Vec3([f64; 3]);
+
+impl Default for Vec3 {
+    fn default() -> Self {
+        Self([0.0; 3])
+    }
+}
+
+impl Vec3 {
+    pub const fn new(elements: [f64; 3]) -> Self {
+        Self(elements)
+    }
+
+    pub const fn x(&self) -> f64 {
+        self.0[0]
+    }
+
+    pub const fn y(&self) -> f64 {
+        self.0[1]
+    }
+
+    pub const fn z(&self) -> f64 {
+        self.0[2]
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.0.iter().map(|&value| value * value).sum()
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn dot(&self, v: &Self) -> f64 {
+        self.0
+            .iter()
+            .zip(v.0)
+            .map(|(left, right)| left * right)
+            .sum()
+    }
+
+    pub const fn cross(&self, v: &Self) -> Self {
+        Self([
+            self.0[1] * v.0[2] - self.0[2] * v.0[1],
+            self.0[2] * v.0[0] - self.0[0] * v.0[2],
+            self.0[0] * v.0[1] - self.0[1] * v.0[0],
+        ])
+    }
+
+    pub fn unit_vector(self) -> Self {
+        self / self.length()
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(mut self) -> Self::Output {
+        self.0.iter_mut().for_each(|value| *value = -*value);
+        self
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f64;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0
+            .iter_mut()
+            .zip(rhs.0)
+            .for_each(|(dest, src)| *dest += src);
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(mut self, rhs: Self) -> Self::Output {
+        self += rhs;
+        self
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.0
+            .iter_mut()
+            .zip(rhs.0)
+            .for_each(|(dest, src)| *dest -= src);
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(mut self, rhs: Self) -> Self::Output {
+        self -= rhs;
+        self
+    }
+}
+
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.0
+            .iter_mut()
+            .zip(rhs.0)
+            .for_each(|(dest, src)| *dest *= src);
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Self;
+
+    fn mul(mut self, rhs: Self) -> Self::Output {
+        self *= rhs;
+        self
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(mut self, rhs: f64) -> Self::Output {
+        self.0.iter_mut().for_each(|value| *value *= rhs);
+        self
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        rhs * self
+    }
+}
+
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, rhs: Self) {
+        self.0
+            .iter_mut()
+            .zip(rhs.0)
+            .for_each(|(dest, src)| *dest /= src);
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(mut self, rhs: f64) -> Self::Output {
+        self.0.iter_mut().for_each(|value| *value /= rhs);
+        self
+    }
+}
+
+impl Display for Vec3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.0[0], self.0[1], self.0[2])
+    }
+}
+
+// Point3 is just an alias for Vec3, but for geometric clarity in the code.
+pub type Point3 = Vec3;
+
+pub type Color = Vec3;
+
+impl From<Color> for Rgb<u8> {
+    fn from(value: Color) -> Self {
+        // Translate the [0, 1) component values to the byte range [0, 255]
+        let red = (255.999 * value.x()) as u8;
+        let green = (255.999 * value.y()) as u8;
+        let blue = (255.999 * value.z()) as u8;
+        Self([red, green, blue])
+    }
+}
