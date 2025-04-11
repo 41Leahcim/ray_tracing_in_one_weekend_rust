cd28cf906a563eca3203b41ec0ca72857ccd7011
Michael Scholten <michael-scholten@hotmail.nl>
2025-04-11 19:11:02 +0200

    Removed the image and added a progress counter, increased image size to make sure the progress is displayed

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use image::{ImageBuffer, Rgb};
 
 fn main() {
-    const IMAGE_WIDTH: u32 = 256;
-    const IMAGE_HEIGHT: u32 = 256;
+    const IMAGE_WIDTH: u32 = 16382;
+    const IMAGE_HEIGHT: u32 = 16382;
     ImageBuffer::from_fn(IMAGE_WIDTH, IMAGE_HEIGHT, |x, y| {
+        if x == 0 {
+            eprint!("\rScanlines remaining: {} ", IMAGE_HEIGHT - y);
+        }
         let red = f64::from(x) / f64::from(IMAGE_WIDTH - 1);
         let green = f64::from(y) / f64::from(IMAGE_HEIGHT - 1);
         let blue = 0;
@@ -11,4 +14,5 @@ fn main() {
     })
     .save("image.ppm")
     .unwrap();
+    eprintln!("\rDone.                 ");
 }
